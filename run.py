82513bf02fc33c5b04c8f1257f5aec8487c92009
luciniv <luciniv.co@gmail.com>
2025-03-19 23:49:21 -0400

    Intents testing

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ ready = True
 class Mantid(commands.Bot):
     def __init__(self):
         # guild, guild_members, guild_messages, and message_content intents, plus all default intents
-        intents = discord.Intents(1 | 32768)
+        intents = discord.Intents(1 | 2 | 32768)
         description = ""
 
         # Create bot instance with command prefix
