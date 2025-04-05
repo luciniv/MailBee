d651cb6a4d033342e4aa86bb578bc676f7dce54a
luciniv <luciniv.co@gmail.com>
2025-04-05 01:10:40 -0400

    Async logging

--- utils/logger.py
+++ utils/logger.py
@@ -1,5 +1,11 @@
+import sys
 from loguru import logger
 
+# Remove default sink
+logger.remove()
+
+# Add threaded (non-blocking) sink
+logger.add(sys.stdout, enqueue=True, backtrace=True, diagnose=True)
 
 # Creates custom logging levels
 logger.level("SYSTEM", no=15, color="\033[38;5;51m\033[48;5;20m\033[1m")
