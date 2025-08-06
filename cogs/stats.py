172b7fd902d2011c7750cf56c13a9d955fedf181
luciniv <luciniv.co@gmail.com>
2025-08-05 21:49:20 -0400

    Fix: Fix leaderboard command (again)

--- cogs/stats.py
+++ cogs/stats.py
@@ -190,11 +190,13 @@ class Stats(commands.Cog):
                     return
 
                 else:
-                    final_result = []
+                    final_result = result
                     # Clean out servers this bot isn't in FIXME not a huge fan of this setup
-                    for entry in result:
-                        if self.bot.get_guild(entry[0]) is not None:
-                            final_result.append(entry)
+                    if type_value in ("open", "duration", "response"):
+                        final_result = []
+                        for entry in result:
+                            if self.bot.get_guild(entry[0]) is not None:
+                                final_result.append(entry)
                     
                     page_counts = build_subsections(len(final_result)) 
                     for page_count in page_counts: 
@@ -207,37 +209,29 @@ class Stats(commands.Cog):
                             statsEmbed.set_footer(text="")
 
                         while (count < limit): 
-                            increment = True
                             row = final_result[count] 
                             
                             if (type_value == "open"):
                                 if not (self.bot.get_guild(row[0]) is None):
                                     statsEmbed.add_field(name="", value=f"{count + 1}) **{(self.bot.get_guild(row[0])).name}**"
                                                         f" - **{row[1]}** ticket(s)", inline=False)
-                                else:
-                                    increment = False
 
                             elif (type_value == "duration"):
                                 if not (self.bot.get_guild(row[0]) is None):
                                     statsEmbed.add_field(name="", value=f"{count + 1}) **{(self.bot.get_guild(row[0])).name}**"
                                                         f" - **{queries.format_time(row[1])}**", inline=False)
-                                else:
-                                    increment = False
 
                             elif (type_value == "response"):
                                 if not (self.bot.get_guild(row[0]) is None):
                                     statsEmbed.add_field(name="", value=f"{count + 1}) **{(self.bot.get_guild(row[0])).name}**"
                                                         f" - **{queries.format_time(row[1])}**", inline=False)
-                                else:
-                                    increment = False
 
                             elif (type_value == "closed"):
                                 statsEmbed.add_field(name="", value=f"{count + 1}) <@{row[0]}> - **{row[1]}** ticket(s)", inline=False)
 
                             elif (type_value == "sent"):
                                 statsEmbed.add_field(name="", value=f"{count + 1}) <@{row[0]}> - **{row[1]}** message(s)", inline=False)
-                            if increment:
-                                count += 1 
+                            count += 1 
                         pages.append(statsEmbed) 
 
             for page in range(len(pages)):
